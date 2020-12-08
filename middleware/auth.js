d1541e0d38742149079c0d1a6718e149ea51f79d
algadaix <alexandre@qured.com>
2020-12-08 22:59:05 +0000

    auth removed

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,20 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
 
-module.exports = function (res, res, next) {
+module.exports = function (req, res, next) {
 	// Get token from header
+
+	const token = req.header('x-auth-token');
+
+	//check if no token
+	if(!token) {
+		return res.status(401).json({msg: "now token, authorization denied"});
+
+		//Verify token
+		try {
+
+		}catch(err) {
+
+		}
+	}
 };
